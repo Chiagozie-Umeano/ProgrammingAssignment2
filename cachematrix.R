7527c82fa1d42c70e0cb7a75e41b01b3fa1d633b
Chiagozie Umeano <agozie2003@gmail.com>
2020-08-01 23:09:17 +0100

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Instructions for testing makeCacheMatrix and CacheSolve functions
+## I set the input x as a matrix
+## then set the solved value "inv" as a null
+## then changed every reference to "mean" to "solve"
 
-## Write a short comment describing this function
+## Set solve value "inv" as a null, and change "mean" to solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  getInverse<-function(){x}
+  setInverse<-function(inverse){inv<<-inverse}
+  getInverse<-function(){inv}
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## same here, changed "mean" to "inverse"
 cacheSolve <- function(x, ...) {
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
         ## Return a matrix that is the inverse of 'x'
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
